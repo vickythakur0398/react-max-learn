088d48cfc1af822477a78f8b1b9653c0fb562374
vickythakur0398 <vickythakur0398@gmail.com>
2020-07-16 23:46:18 +0530

    understanding jsx p-1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 //..here we have come form rnder kyuki isi ko kar rha h refer
 //so this is our 
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  // return (
+  //   <div className="App">
      
-       <h1>
-         HII, this is first commit
-       </h1>
-    </div>
+  //      <h1>
+  //        HII, this is first commit
+  //      </h1>
+  //   </div>
+//we will se that how that code which we have commented works 
+
+  return(
+    //here crateElement can take unlimited agruments but min 3 is necessary
+  React.createElement('div' ,null, 'h1' , 'here neither styling nor h1 tag will work')
   );
 }
 
