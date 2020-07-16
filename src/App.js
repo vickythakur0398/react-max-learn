79fe70565a364721212dce0bded59d9feb56b2b0
vickythakur0398 <vickythakur0398@gmail.com>
2020-07-16 23:52:33 +0530

    with styling understanded jsx p-3 done

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
 
   return(
     //here crateElement can take unlimited agruments but min 3 is necessary
-  React.createElement('div' ,null , React.createElement('h1', null, 'so that is in h1 but w/o styling'))
+    //now we will also apply style class HERE we are applying the same style which is commented above
+    //but ye likhna is quite cumberscent so we undestands now how internally the above things works
+  React.createElement('div' ,{className: 'App'} , React.createElement('h1', null, 'so that is in h1 but with styling'))
   );
 }
 
